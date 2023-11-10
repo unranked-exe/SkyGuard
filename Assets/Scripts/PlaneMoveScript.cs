ac38aa57f8fdb509ff5d04f7a547eb65292d547a
unranked-exe <120649405+unranked-exe@users.noreply.github.com>
2023-11-10 23:46:13 +0000

    Milestone 2 Floating Text Update
    Not using prefab of floating text but instead combined it into my plane prefab instead. I also added code to output the current bearing of the plane.
    Working on getting outputting a random plane number to floating text

--- Assets/Scripts/PlaneMoveScript.cs
+++ Assets/Scripts/PlaneMoveScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlaneMoveScript : MonoBehaviour
@@ -8,8 +9,17 @@ public class PlaneMoveScript : MonoBehaviour
     [SerializeField] private Rigidbody2D _PlaneRB;
     //Reference to the speed of the plane
     [SerializeField] private float _PlaneSpeed;
+    
+    //Reference to the floating text game object
+    [SerializeField] private GameObject floatingText;
+    //Reference to the text mesh component of the floating text
+    [SerializeField] private TextMeshPro textmesh;
+
     private void Awake()
     {
+        //Set the rotation of the Floating Text to upright
+        floatingText.transform.rotation = Quaternion.identity;
+        OutputBearing();
         //Call the MovePlane function
         MovePlane();
     }
@@ -21,8 +31,17 @@ public class PlaneMoveScript : MonoBehaviour
         _PlaneRB.velocity = transform.up * _PlaneSpeed;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    //Function to output the bearing of the plane for Flaoting Text
+    void OutputBearing()
     {
-        if collision.gameObject
+        //Get the rotation of the plane
+        int rot = Mathf.RoundToInt(transform.rotation.eulerAngles.z);
+        //Calcualtes the real bearing of the plane
+        rot = 360 - rot;
+        //Set the text of the floating text to the rotation
+        textmesh.text = rot.ToString() + "°";
     }
 }
+/* Debug.Log(rot);
+        rot = 360 - rot;
+        Debug.Log(rot);*/
--- Assets/Scripts/SpawnerScript.cs
+++ Assets/Scripts/SpawnerScript.cs
@@ -14,11 +14,9 @@ public class SpawnerScript : MonoBehaviour
 
     // This is an array of the spawnable points.
     private static Vector2[] spawnablePoints = new Vector2[] {new Vector2(0, 8), new Vector2(11, 0), new Vector2(-11, 0)};
-
-    // This is a reference to the floating text prefab.
-    [SerializeField] private GameObject floatingText;
-
-
+    // This is an array of the airline prefixes.
+    [SerializeField] private static string[] airlinePrefix = new string[4] {"AI", "BA", "EY", "SV"};
+    
     void Start()
     {
         // This starts the coroutine that will spawn the planes.
@@ -32,14 +30,15 @@ public class SpawnerScript : MonoBehaviour
         {
             // This waits for the specified amount of time.
             yield return new WaitForSeconds(_spawnInterval);
-            GameObject planeInstance;
-            // This instantiates plane prefab
-            planeInstance = Instantiate(plane, transform.position, Quaternion.Euler(0, 0, RotationControl()));
-            Instantiate(floatingText, transform.position, Quaternion.identity).transform.SetParent(planeInstance.transform);
+            
+            // This instantiates plane prefab while also setting this instance to a variable.
+            GameObject instance =  Instantiate(plane, transform.position, Quaternion.Euler(0, 0, RotationControl()));
+            instance.name = PlaneNamer();
+            //Shifts the position for spawner for next spawning
             transform.position = MoveSpawner();
         }
     }
-
+    
     // This method will move the spawner to a random point on the screen.
     private Vector2 MoveSpawner()
     {
@@ -74,5 +73,18 @@ public class SpawnerScript : MonoBehaviour
         }
         //Return the randomised rotation within the specified range
         return rot;
+    }
+
+    // This method will return the name of the plane.
+    private string PlaneNamer()
+    {
+        //Local variable to store the name of the plane
+        string newName;
+        //Randomly choose an Airline prefix
+        newName = airlinePrefix[Random.Range(0, 4)];
+        //Add a random flight number to the end of the prefix
+        newName += Random.Range(0, 999).ToString();
+        //Return the name of the plane
+        return newName;
     }
 }
