26c44a2bfc6d037a6aa52c47c7bb00392024bb48
unranked-exe <120649405+unranked-exe@users.noreply.github.com>
2023-11-14 23:41:39 +0000

    GameMnager script updates

--- Assets/Scripts/Game Manager.cs
+++ Assets/Scripts/Game Manager.cs
@@ -1,32 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    //Reference to the instance of the GameManager
     public static GameManager instance;
+
+    //Variable to the current state of the game.
+    public GameState State;
 
     private void Awake()
     {
+        //Sets the instance of the GameManager to this.
         instance = this;
     }
 
-
-    // Start is called before the first frame update
-    void Start()
+    public void UpdateGameState(GameState newState)
     {
-        
+        //Sets the new state of game to current state.
+        State = newState;
+
+        //Switch statement that checks the new state of the game.
+        switch(newState)
+        { 
+            case GameState.Playing:
+                //Do something
+                break;
+            case GameState.Paused:
+                //Do something
+                break;
+            case GameState.GameOver:
+                //Do something
+                break;
+            default:
+                //For debugging purposes in later stages of development.
+                throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
-
-    private void Collision ()
+    //Enumertor that holds the different states of the game.
+    public enum GameState
     {
-        
+        Playing,
+        Paused,
+        GameOver
     }
 }
